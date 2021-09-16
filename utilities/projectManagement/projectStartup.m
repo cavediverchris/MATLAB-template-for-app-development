23907fdc1933bb44a8ea716965086f81d9fcd8a4
chris <chris@DESKTOP-36LNQQM.localdomain>
2021-09-16 12:44:07 +0100

    initial commit of very basic folder structure and files from legacy template

---
+++ utilities/projectManagement/projectStartup.m
@@ -0,0 +1,18 @@
+%% PROJECT SETUP Establish environment
+%   The projectStartup script is designed to run on Project Start to setup
+%   local customisations and to prepare the MATLAB Interactive
+%   Development Environment (IDE) ready for work.
+%
+%   FUNCTION INPUTS
+%       None
+%
+%   FUNCTION OUTPUTS
+%       None
+%
+%   OTHER OUTPUTS
+%       None
+%
+%   LINKS
+%   For further information check out: <a href="matlab:web('https://github.com/cavediverchris/Project-Template-for-Apps/tree/standardise-file-documentation#project-management---projectstartup')">Git documentation</a>.
+%
+%% FUNCTION
